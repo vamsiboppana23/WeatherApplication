1cb85c2df0bfd065287650430c59393b392205e0
vamsiboppana23 <vamsi.b231@gmail.com>
2025-05-07 16:23:41 -0400

    sync changes to qa

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
 resource "aws_s3_bucket" "weatherapp" {
-  bucket = "my-tf-weatherapp-bucket"
+  bucket = "my-tf-weatherapp-qa-bucket"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
+}
+# Create a CloudWatch Log Group
+resource "aws_cloudwatch_log_group" "example_log_group" {
+  name              = "/aws/lambda/example-log-group"
+  retention_in_days = 14
 }
